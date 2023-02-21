5a202747bc7e9f68daad04c9dd27a1a52861812b
Jose Andres Salazar <joseandres61@gmail.com>
2023-02-21 19:30:49 -0400

    fix dotenv config place

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const loggerMiddleware = require('./middleware/request-logger.mid');
-const ImageRouter = require('./routes/image.route');
+const { ImageRouter } = require('./routes/image.route');
 const ErrorHandlerMiddleware = require('./middleware/error-handler-mid');
-
-require('dotenv').config();
+const { connectDB } = require('./config/connectDB');
+const { cloudinaryConfig } = require('./config/cloudinaryConfig');
 
 app.use(cors());
-app.use(json());
+app.use(express.json());
 app.use(loggerMiddleware);
-app.use('/api', ImageRouter);
 
 app.use(ErrorHandlerMiddleware);
+
+app.use('/api', ImageRouter);
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is runnig on port: ${port}`);
 });
+
+connectDB();
+cloudinaryConfig();
